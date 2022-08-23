8c5931451805110735050b4cfeefdb3a255345ee
BOWEI KANG <kangbw702@gmail.com>
2022-08-22 22:21:11 -0400

    update main_apa_test.R

--- R/apa_testing.R
+++ R/apa_testing.R
@@ -1,3 +1,20 @@
 
 # test differential APA events between two cell groups
 
+single_gene_test_1vsothers <- function(gene.to.test, counts, anno.tbl, attr.tbl, ctype.colname, base_grp, cut.low.pct = 0.05) {
+
+  # find associated peaks
+  peak.to.test = rownames(anno.tbl)[which(anno.tbl$gene == gene.to.test)]
+  # order by mode.pos (small -> large)
+  peak.to.test = peak.to.test[order(anno.tbl[peak.to.test, "mode.pos"])]
+  strand = utils::head(anno.tbl[peak.to.test, 'strand'], 1)
+  mode.pos = anno.tbl[peak.to.test, 'mode.pos']
+
+  # generate df.test
+  df.test = data.frame(cellid = colnames(counts), cl = attr.tbl[, ctype.colname], stringsAsFactors = FALSE)
+  for (i in 1:length(peak.to.test)) df.test[, paste0("raw|", peak.to.test[i])] = counts[peak.to.test[i],]
+  df.test[, paste0("raw|", gene.to.test)] = rowSums(df.test[, 3:(3+length(peak.to.test)-1)])
+
+
+
+}
--- R/main_apa_test.R
+++ R/main_apa_test.R
@@ -1,2 +1,99 @@
 
-# test for APA differential events between two cell groups
+# test for APA differential events between two cell groups (all legitimate genes/3'UTRs)
+
+#' Differential APA testing
+#'
+#' @param counts.dir Directory for count matrix and corresponding peak site names and cell barcodes.
+#' @param attr.file Directory for the cell attributes table.
+#' @param anno.file Directory for the peak annotation table.
+#' @param utr3.file Directory for the 3'UTR annotation table (mapping each 3'UTR region to a unique transcript ID).
+#' @param ctype.colname Column name that defines cell groups in the cell attributes table.
+#' @param base_grp Base cell group name.
+#' @param alt_grp Alternative cell group name. If 'Other', take all cell groups except base as alternative.
+#' @param cut.low.pct Threshold for low expressing percentage.
+#' @param test.type Type of testing. Value can be 'gene', 'utr', 'btw'.
+#'
+#' @return Differential APA testing result table.
+#' @export
+#'
+#' @examples
+#' ls()
+#'
+#'
+#'
+#'
+apa_test <- function(counts.dir,
+                     attr.file,
+                     anno.file,
+                     utr3.file,
+                     ctype.colname,
+                     base_grp,
+                     alt_grp,
+                     cut.low.pct = 0.05,
+                     test.type
+                     )
+  {
+
+  # prepare the subset of counts, attr, anno tables that cope with the test
+
+  cnt = Matrix::readMM(paste0(counts.dir, '/matrix.mtx'))
+  colnames(cnt) = unlist(utils::read.table(paste0(counts.dir, '/barcodes.tsv'), sep = '\t', header = F))
+  rownames(cnt) = unlist(utils::read.table(paste0(counts.dir, '/sitenames.tsv'), sep = '\t', header = F))
+
+  # manipulate anno.tbl
+  anno.tbl = utils::read.csv(anno.file, stringsAsFactors = FALSE)
+
+  # assign each peak a Transcript.ID
+  anno.tbl$Transcript.ID = sapply(1:nrow(anno.tbl), assign.trans.ID, utr3.file = utr3.file, anno.tbl = anno.tbl)
+
+  # add summary of number of peaks/transcripts
+  gene = gene.trans.ID = NULL
+  anno.tbl$gene.trans.ID = paste0(anno.tbl$gene, ":", anno.tbl$Transcript.ID)
+  anno.tbl.agg1 = anno.tbl %>% dplyr::group_by(gene) %>% dplyr::summarise(nn.pk = dplyr::n())
+  anno.tbl.agg2 = anno.tbl %>% dplyr::group_by(gene.trans.ID) %>% dplyr::summarise(nn.pk.per.trans = dplyr::n(), gene = utils::head(gene, 1))
+  anno.tbl.agg3 = anno.tbl.agg2 %>% dplyr::group_by(gene) %>% dplyr::summarise(nn.trans.per.gene = dplyr::n())
+  anno.tbl = anno.tbl %>% dplyr::left_join(anno.tbl.agg1, by = 'gene') %>% dplyr::left_join(anno.tbl.agg2[, 1:2], by = 'gene.trans.ID')  %>% dplyr::left_join(anno.tbl.agg3, by = 'gene')
+
+  rownames(anno.tbl) = anno.tbl$polyA_ID
+
+  message('Peak site names in anno table are consistent with count matrix? ', all(rownames(cnt) == rownames(anno.tbl)))
+
+  # manipulate attr.tbl
+  attr.tbl = utils::read.csv(attr.file, stringsAsFactors = FALSE, row.names = 1)
+  attr.tbl = attr.tbl[!duplicated(attr.tbl$cellbc), ]
+  rownames(attr.tbl) = attr.tbl$cellbc
+  shared.cb = intersect(colnames(cnt), rownames(attr.tbl))
+  attr.tbl = attr.tbl[shared.cb, ]
+  cnt = cnt[, shared.cb]
+
+  message('Cell barcodes in attr table are consistent with count matrix? ', all(colnames(cnt) == rownames(attr.tbl)))
+
+
+
+}
+
+
+assign.trans.ID <- function(pid, utr3.file, anno.tbl) {
+
+  utr3.anno = utils::read.csv(utr3.file, stringsAsFactors = FALSE, row.names = 1)
+  ids = which(utr3.anno$gene_symbol == anno.tbl[pid, 'gene'] & utr3.anno$start <= anno.tbl[pid, 'mode.pos'] & utr3.anno$end >= anno.tbl[pid, 'mode.pos'])
+
+  if (length(ids) >  0) return(as.character(utr3.anno$Transcript.ID[ids]))
+
+  if (length(ids) == 0) { # assign ID of the closest downstream 3'UTR
+
+    tmp = utr3.anno[utr3.anno$gene_symbol == anno.tbl[pid, 'gene'], ]
+    tmp = tmp[order(tmp$start), ]
+
+    if (anno.tbl[id, 'strand'] == '-') {
+      id = max(which(anno.tbl[pid, 'mode.pos'] - tmp$end > 0))
+    } else {
+      id = min(which(tmp$start - anno.tbl[pid, 'mode.pos'] > 0))
+    }
+
+    return(as.character(tmp$Transcript.ID[pid]))
+  } # na ids
+
+}
+
+
